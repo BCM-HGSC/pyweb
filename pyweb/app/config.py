5c7f858ce4bab3fc2108461bd3d08a997532ca45
Walker Hale IV <walker.hale.iv@gmail.com>
2018-09-05 15:12:54 -0500

    Configure logging

--- pyweb/app/config.py
+++ pyweb/app/config.py
@@ -1,11 +1,44 @@
+import logging
+import logging.config
 import os
-from sys import stderr
 
-
-print('hello from /app/config.py', file=stderr)
 
 bind = '0.0.0.0:8000'
 pidfile = '/var/run/gunicorn.pid'
+
+# See: http://docs.gunicorn.org/en/latest/settings.html#logconfig-dict
+logconfig_dict = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'basic': {
+            'class': 'logging.Formatter',
+            'format': '%(asctime)s [%(process)d] [%(levelname)s] %(message)s',
+            'datefmt': '[%Y-%m-%d %H:%M:%S %z]'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'basic',
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console']
+    },
+    'loggers': {
+        'asyncio': {},
+        'concurrent.futures': {},
+        'gunicorn': {},
+        'gunicorn.access': {'level': 'INFO', 'propagate': False},
+        'gunicorn.error': {'level': 'INFO', 'propagate': True},
+        'gunicorn.http': {},
+        'gunicorn.http.wsgi': {},
+    },
+}
+
+logging.info('Hello from /app/config.py on %d', os.getpid())
 
 
 # Load any relevant environment variables as settings...
@@ -21,4 +54,6 @@ for k, v in os.environ.items():
             value = True
         key = k.split('_', 1)[1].lower()
         globals()[key] = value
-        print('{}={!r}'.format(key, value), file=stderr)
+        logging.info('%s=%r', key, value)
+
+logging.info('Leaving /app/config.py')
